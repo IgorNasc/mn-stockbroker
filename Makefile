24296e71b3df1b948b2fa09d0cd2f367540db993
Igor <igornascimento060@gmail.com>
2021-07-28 17:41:44 -0300

    Added lombok and makefile

---
+++ Makefile
@@ -0,0 +1,11 @@
+clean:
+	mvn clean
+
+install:
+	mvn clean install
+
+generate-sources:
+	mvn clean generate-sources
+
+test:
+	mvn clean test
--- src/main/java/com/igor/micronaut/udemy/GreetingConfig.java
+++ src/main/java/com/igor/micronaut/udemy/GreetingConfig.java
@@ -2,25 +2,20 @@ package com.igor.micronaut.udemy;
 
 import io.micronaut.context.annotation.ConfigurationInject;
 import io.micronaut.context.annotation.ConfigurationProperties;
+import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
 @ConfigurationProperties("hello.config.greetings")
 public class GreetingConfig {
+    @Getter
     final String de;
+    @Getter
     final String en;
 
     @ConfigurationInject
     public GreetingConfig(@NotBlank String de,@NotBlank String en) {
         this.de = de;
         this.en = en;
-    }
-
-    public String getDe() {
-        return de;
-    }
-
-    public String getEn() {
-        return en;
     }
 }
